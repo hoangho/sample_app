a12506a435d4fb6a6dafc11758805f96dc8dd66d
hoangho <hoang.ho@techpropulsionlabs.com>
2013-03-11 11:44:50 +0700

    finished basic microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,10 +36,8 @@ class User < ActiveRecord::Base
 
 
 
-  def self.search(search, page)
-    paginate :per_page => 5, :page => page,
-           :conditions => ['name like ?', "%#{search}%"],
-           :order => 'name'
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
 
